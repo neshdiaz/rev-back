c72bff76385db29827a70d8e1f87545300c0fe5a
neshdiaz <nesh.diaz@gmail.com>
2024-07-09 14:56:08 -0500

    updated compras controllers and services

--- rev-digitales-backend/src/compras/compras.controller.ts
+++ rev-digitales-backend/src/compras/compras.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
-
+import { CreateCompraDto } from './dto/create-compra.dto';
+import { UpdateCompraDto } from './dto/update-compra.dto';
+import {
+  Controller,
+  Post,
+  Get,
+  Delete,
+  Patch,
+  Body,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ComprasService } from './compras.service';
 @Controller('compras')
-export class ComprasController {}
+export class ComprasController {
+  constructor(private comprasService: ComprasService) {}
+
+  @Post()
+  createCompra(@Body() newCompra: CreateCompraDto) {
+    return this.comprasService.createCompra(newCompra);
+  }
+
+  @Get()
+  getCompras() {
+    return this.comprasService.getCompras();
+  }
+
+  @Get(':id')
+  getCompra(@Param('id', ParseIntPipe) id: number) {
+    return this.comprasService.getCompra(id);
+  }
+
+  @Delete(':id')
+  deleteCompra(@Param('id', ParseIntPipe) id: number) {
+    return this.comprasService.deleteCompra(id);
+  }
+
+  @Patch(':id')
+  updateCompra(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() compra: UpdateCompraDto,
+  ) {
+    return this.comprasService.updateCompra(id, compra);
+  }
+}
--- rev-digitales-backend/src/compras/compras.service.ts
+++ rev-digitales-backend/src/compras/compras.service.ts
@@ -1,4 +1,63 @@
 import { Injectable } from '@nestjs/common';
+import { Compra } from './compra.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateCompraDto } from './dto/create-compra.dto';
+import { UpdateCompraDto } from './dto/update-compra.dto';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Injectable()
-export class ComprasService {}
+export class ComprasService {
+  constructor(
+    @InjectRepository(Compra) private compraRepository: Repository<Compra>,
+  ) {}
+  async createCompra(compra: CreateCompraDto) {
+    const newCompra = this.compraRepository.create(compra);
+    return this.compraRepository.save(newCompra);
+  }
+
+  getCompras(): Promise<Compra[]> {
+    return this.compraRepository.find();
+  }
+
+  async getCompra(id: number) {
+    const compraFound = await this.compraRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!compraFound) {
+      throw new HttpException(
+        'Este id de compra no existe',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return compraFound;
+  }
+
+  async deleteCompra(id: number) {
+    const result = await this.compraRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new HttpException('El elemento no existe', HttpStatus.NOT_FOUND);
+    }
+    return result;
+  }
+
+  async updateCompra(id: number, compra: UpdateCompraDto) {
+    const compraFound = await this.compraRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!compraFound) {
+      throw new HttpException(
+        'Este id de compra no existe',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return this.compraRepository.update(id, compra);
+  }
+}
